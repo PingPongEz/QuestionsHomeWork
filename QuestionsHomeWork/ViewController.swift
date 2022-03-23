11461490ae426baca6be26973a050aeb8388f0a3
Sergey <kopgamerk@gmail.com>
2022-03-23 12:57:57 +0300

    Initial Commit

---
+++ QuestionsHomeWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuestionsHomeWork
+//
+//  Created by u on 23.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
